88177fc141c5881dcb10c1ede95e017935f5dfef
Jean Saitou <jeansaitou1234@gmail.com>
2024-01-23 15:02:21 -0500

    Ajout de variable concernant la voiture

--- Carrefour_Exercice_Git_Avance/Form1.cs
+++ Carrefour_Exercice_Git_Avance/Form1.cs
@@ -2,9 +2,22 @@ namespace Carrefour_Exercice_Git_Avance
 {
     public partial class Form1 : Form
     {
+        public string Couleur;
+        public int AnneeConstruction;
+        public int Vitesse = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Accelerer()
+        {
+            Vitesse += 2;
+        }
+
+        public void Freiner()
+        {
+            Vitesse -= 1;
         }
     }
 }
